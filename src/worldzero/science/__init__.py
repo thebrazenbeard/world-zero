fdba7d3657b3c51b206661d32ad06b7caf2be407
Patrick Sims <234958733+thebrazenbeard@users.noreply.github.com>
2026-09-17 15:13:49 -0400

    feat: expose scientific control primitives

---
+++ src/worldzero/science/__init__.py
@@ -0,0 +1,22 @@
+"""Scientific-control primitives for World Zero."""
+
+from .canonical import canonical_json_bytes, content_digest
+from .types import (
+    AdmissionDisposition,
+    BenchmarkResultLabel,
+    ControlMode,
+    EvidenceClass,
+    IdentifiabilityClass,
+    StructuralResultLabel,
+)
+
+__all__ = [
+    "AdmissionDisposition",
+    "BenchmarkResultLabel",
+    "ControlMode",
+    "EvidenceClass",
+    "IdentifiabilityClass",
+    "StructuralResultLabel",
+    "canonical_json_bytes",
+    "content_digest",
+]
